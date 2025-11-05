d2605a567eb2edaea7ef905283f90eeac911bc85
nityam <nixxspeaks@gmail.com>
2025-11-05 23:18:10 +0530

    Time: 11 ms (76.85%) | Memory: 32.8 MB (90.03%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         int n = nums.size();
         vector<int> ans;
-        for (int i = 0; i + k <= n; i++) {
-            vector<int> f(51,0);
-            for (int j = i; j < i + k; j++) f[nums[j]]++;
+        vector<int> f(51,0);
+        for (int i = 0; i < k; i++) f[nums[i]]++;
+        auto calc = [&]() {
             vector<pair<int,int>> v;
             for (int val = 1; val <= 50; val++) if (f[val]) v.push_back({f[val], val});
             sort(v.begin(), v.end(), [](auto &a, auto &b){
@@ -14,7 +14,13 @@ public:
             });
             int sum = 0;
             for (int t = 0; t < (int)v.size() && t < x; t++) sum += v[t].first * v[t].second;
-            ans.push_back(sum);
+            return sum;
+        };
+        ans.push_back(calc());
+        for (int i = k; i < n; i++) {
+            f[nums[i-k]]--;
+            f[nums[i]]++;
+            ans.push_back(calc());
         }
         return ans;
     }
