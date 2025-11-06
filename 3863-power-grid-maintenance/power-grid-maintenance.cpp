f839d1d7f18361f43b68a0a80c43cdd79f870a8c
nityam <nixxspeaks@gmail.com>
2025-11-06 23:22:43 +0530

    Time: 184 ms (87.15%) | Memory: 325.9 MB (94.18%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/power-grid-maintenance.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
-        
-        vector<int> parent(c + 1), size(c + 1, 1);
+        vector<int> parent(c + 1), sz(c + 1, 1);
         iota(parent.begin(), parent.end(), 0);
+
         function<int(int)> find = [&](int x) {
             return parent[x] == x ? x : parent[x] = find(parent[x]);
         };
         auto unite = [&](int a, int b) {
-            a = find(a); b = find(b);
+            a = find(a), b = find(b);
             if (a == b) return;
-            if (size[a] < size[b]) swap(a, b);
+            if (sz[a] < sz[b]) swap(a, b);
             parent[b] = a;
-            size[a] += size[b];
+            sz[a] += sz[b];
         };
-        
-       
+
         for (auto &e : connections) unite(e[0], e[1]);
 
-        unordered_map<int, set<int>> compOnline;
-        for (int i = 1; i <= c; ++i)
-            compOnline[find(i)].insert(i);
+        // Priority queue per component (min-heap)
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> pq;
+        for (int i = 1; i <= c; ++i) pq[find(i)].push(i);
 
         vector<bool> online(c + 1, true);
-        vector<int> result;
- 
+        vector<int> ans;
+
         for (auto &q : queries) {
             int type = q[0], x = q[1];
             int root = find(x);
 
             if (type == 1) {
-                if (online[x]) result.push_back(x);
+                if (online[x]) ans.push_back(x);
                 else {
-                    if (compOnline[root].empty()) result.push_back(-1);
-                    else result.push_back(*compOnline[root].begin());
+                    // Lazy removal of offline nodes
+                    while (!pq[root].empty() && !online[pq[root].top()])
+                        pq[root].pop();
+
+                    if (pq[root].empty()) ans.push_back(-1);
+                    else ans.push_back(pq[root].top());
                 }
             } else {
-                if (online[x]) {
-                    online[x] = false;
-                    compOnline[root].erase(x);
-                }
+                online[x] = false;
             }
         }
-
-        return result;
+        return ans;
     }
 };
