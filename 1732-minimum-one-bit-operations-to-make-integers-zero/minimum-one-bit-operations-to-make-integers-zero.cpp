ad2626de2c45f4e6f5a06ee6f4c2c26362bbadbb
nityam <nixxspeaks@gmail.com>
2025-11-08 23:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (5.98%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
+    unordered_map<int,int> dp;
     int minimumOneBitOperations(int n) {
-        return n ^ (n >> 1) ^ (n >> 2) ^ (n >> 3) ^ (n >> 4)
-               ^ (n >> 5) ^ (n >> 6) ^ (n >> 7) ^ (n >> 8)
-               ^ (n >> 9) ^ (n >> 10) ^ (n >> 11) ^ (n >> 12)
-               ^ (n >> 13) ^ (n >> 14) ^ (n >> 15) ^ (n >> 16)
-               ^ (n >> 17) ^ (n >> 18) ^ (n >> 19) ^ (n >> 20)
-               ^ (n >> 21) ^ (n >> 22) ^ (n >> 23) ^ (n >> 24)
-               ^ (n >> 25) ^ (n >> 26) ^ (n >> 27) ^ (n >> 28)
-               ^ (n >> 29) ^ (n >> 30);
+        if (n == 0) return 0;
+        if (dp.count(n)) return dp[n];
+        int b = 0;
+        while ((1 << (b + 1)) <= n) b++;
+        return dp[n] = ((1 << (b + 1)) - 1) - minimumOneBitOperations(n ^ (1 << b));
     }
 };
