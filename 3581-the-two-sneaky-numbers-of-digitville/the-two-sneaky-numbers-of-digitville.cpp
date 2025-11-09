f0d06c741bfd0c92d39b6adb6fe52b371e680f5c
nityam <nixxspeaks@gmail.com>
2025-11-09 23:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (71.62%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> freq(n, 0), res;
-        for (int x : nums) freq[x]++;
-        for (int i = 0; i < n; ++i)
-            if (freq[i] == 2) res.push_back(i);
-        return res;
+        int n = nums.size() - 2;
+        long long S = 1LL * n * (n - 1) / 2;
+        long long P = 1LL * (n - 1) * n * (2LL * n - 1) / 6;
+
+        long long sumNums = 0, sumSqNums = 0;
+        for (int x : nums) {
+            sumNums += x;
+            sumSqNums += 1LL * x * x;
+        }
+
+        long long sumDiff = sumNums - S;      
+        long long sqDiff = sumSqNums - P;    
+        long long prod = ((sumDiff * sumDiff) - sqDiff) / 2; 
+
+        long long D = sqrt(sumDiff * sumDiff - 4 * prod);
+        long long x = (sumDiff + D) / 2;
+        long long y = (sumDiff - D) / 2;
+
+        return {(int)x, (int)y};
     }
 };
