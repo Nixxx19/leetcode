9c738f8929905f7f3877a283dbc0e5b9ccc9adc1
nityam <nixxspeaks@gmail.com>
2025-11-09 23:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (54.64%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_map<int,int> freq;
+        int n = nums.size();
+        vector<int> freq(n, 0), res;
         for (int x : nums) freq[x]++;
-        vector<int> res;
-        for (auto& [k, v] : freq)
-            if (v == 2) res.push_back(k);
+        for (int i = 0; i < n; ++i)
+            if (freq[i] == 2) res.push_back(i);
         return res;
     }
 };
