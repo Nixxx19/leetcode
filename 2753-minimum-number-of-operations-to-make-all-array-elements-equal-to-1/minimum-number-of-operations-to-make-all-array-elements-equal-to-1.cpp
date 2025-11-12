398dfbd39ae7bd5fc78424fec7dd05245db4b940
nityam <nixxspeaks@gmail.com>
2025-11-12 21:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 31.9 MB (9.03%) - LeetSync

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -3,12 +3,13 @@ public:
     int minOperations(vector<int>& nums) {
         int n = nums.size();
         int ones = count(nums.begin(), nums.end(), 1);
-        if (ones > 0) return n - ones;
+        if (ones) return n - ones;
 
-        int minLen = n + 1;
+        int minLen = INT_MAX;
         for (int i = 0; i < n; i++) {
             int g = nums[i];
-            for (int j = i; j < n; j++) {
+            if (g == 1) return n - 1;
+            for (int j = i + 1; j < n; j++) {
                 g = gcd(g, nums[j]);
                 if (g == 1) {
                     minLen = min(minLen, j - i + 1);
@@ -16,7 +17,6 @@ public:
                 }
             }
         }
-        if (minLen == n + 1) return -1;
-        return (minLen - 1) + (n - 1);
+        return (minLen == INT_MAX) ? -1 : (minLen - 1 + n - 1);
     }
 };
