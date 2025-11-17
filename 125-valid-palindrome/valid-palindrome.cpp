cbf60a6fb6c7acc3b21cfce06a0df8d0a73f75a2
nityam <nixxspeaks@gmail.com>
2025-11-17 23:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string t;
-        t.reserve(s.size());
-        for (char c : s)
-            if (isalnum(c)) t.push_back(tolower(c));
-        int l = 0, r = t.size() - 1;
-        while (l < r) if (t[l++] != t[r--]) return false;
+        for (int i = 0, j = s.size() - 1; i < j; ) {
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            i++; j--;
+        }
         return true;
     }
 };
