bdd3bc63d06b3d42ffb8cab34f41f669f4d34f44
nityam <nixxspeaks@gmail.com>
2025-11-20 22:56:10 +0530

    Time: 4 ms (19.88%) | Memory: 28.3 MB (7.87%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,7 +3,15 @@ class Solution
 public:
     int majorityElement(vector<int>& nums)
     {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        int ans = 0;
+        for (int b = 0; b < 32; b++)
+        {
+            int c = 0;
+            for (int v : nums)
+                if (v >> b & 1) c++;
+            if (c > nums.size()/2)
+                ans |= (1 << b);
+        }
+        return ans;
     }
 };
