63aee12e369a618ff94f56373b2bcc233e499392
nityam <nixxspeaks@gmail.com>
2025-11-22 22:11:12 +0530

    Time: 4 ms (29.76%) | Memory: 8.3 MB (40.94%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        unsigned int ans = 0;
-        for (int i = 0; i < 32; i++) {
-            if (n & (1 << i))
-                ans |= 1 << (31 - i);
-        }
-        return ans;
+        unsigned int x = (unsigned int)n;
+        x = (x >> 16) | (x << 16);
+        x = ((x & 0xff00ff00) >> 8)  | ((x & 0x00ff00ff) << 8);
+        x = ((x & 0xf0f0f0f0) >> 4)  | ((x & 0x0f0f0f0f) << 4);
+        x = ((x & 0xcccccccc) >> 2)  | ((x & 0x33333333) << 2);
+        x = ((x & 0xaaaaaaaa) >> 1)  | ((x & 0x55555555) << 1);
+        return x;
     }
 };
