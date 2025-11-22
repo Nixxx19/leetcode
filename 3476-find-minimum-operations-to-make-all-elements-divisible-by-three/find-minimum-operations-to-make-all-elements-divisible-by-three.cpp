1df128ef4ed914bbbd2b5ebccd842fccadc4e93e
nityam <nixxspeaks@gmail.com>
2025-11-22 22:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (45.48%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         int ops = 0;
-        for (int x : nums) {
-            if (x % 3 == 1) ops++;
-            else if (x % 3 == 2) ops++;
-        }
+        for (int x : nums) ops += (x % 3 != 0);
         return ops;
     }
 };
