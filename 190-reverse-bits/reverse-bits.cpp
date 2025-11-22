466113bf54deb3dca400ab97f7a7e3bcd98b78f4
nityam <nixxspeaks@gmail.com>
2025-11-22 22:11:01 +0530

    Time: 4 ms (29.76%) | Memory: 8.1 MB (69.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,9 +3,8 @@ public:
     int reverseBits(int n) {
         unsigned int ans = 0;
         for (int i = 0; i < 32; i++) {
-            ans <<= 1;
-            ans |= (n & 1);
-            n >>= 1;
+            if (n & (1 << i))
+                ans |= 1 << (31 - i);
         }
         return ans;
     }
