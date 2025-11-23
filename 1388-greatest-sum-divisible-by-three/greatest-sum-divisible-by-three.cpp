e048712ed5421f273f5203bb6f44b81af1e7522a
nityam <nixxspeaks@gmail.com>
2025-11-23 18:45:15 +0530

    Time: 40 ms (33.86%) | Memory: 37 MB (43.08%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        int sum = 0;
-        vector<int> r1, r2;
-
+        vector<int> dp(3, INT_MIN);
+        dp[0] = 0;
+        
         for (int x : nums) {
-            sum += x;
-            if (x % 3 == 1) r1.push_back(x);
-            else if (x % 3 == 2) r2.push_back(x);
+            vector<int> ndp(dp);
+            for (int r = 0; r < 3; r++) {
+                int nr = (r + x) % 3;
+                ndp[nr] = max(ndp[nr], dp[r] + x);
+            }
+            dp = ndp;
         }
-
-        sort(r1.begin(), r1.end());
-        sort(r2.begin(), r2.end());
-
-        if (sum % 3 == 0) return sum;
-
-        int res = 0;
-
-        if (sum % 3 == 1) {
-            int remove1 = r1.size() > 0 ? r1[0] : INT_MAX;
-            int remove2 = r2.size() > 1 ? r2[0] + r2[1] : INT_MAX;
-            res = sum - min(remove1, remove2);
-        } 
-        else { // sum % 3 == 2
-            int remove1 = r2.size() > 0 ? r2[0] : INT_MAX;
-            int remove2 = r1.size() > 1 ? r1[0] + r1[1] : INT_MAX;
-            res = sum - min(remove1, remove2);
-        }
-
-        return res < 0 ? 0 : res;
+        
+        return dp[0];
     }
 };
