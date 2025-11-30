8943fdaf187bb358c2e5fa5f21e1e00cc66187b1
nityam <nixxspeaks@gmail.com>
2025-11-30 23:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (8.18%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        vector<int> m1(256, -1), m2(256, -1);
+        unordered_map<char,char> m1, m2;
         for (int i = 0; i < s.size(); i++) {
-            if (m1[s[i]] != m2[t[i]]) return false;
-            m1[s[i]] = m2[t[i]] = i;
+            if (m1.count(s[i]) && m1[s[i]] != t[i]) return false;
+            if (m2.count(t[i]) && m2[t[i]] != s[i]) return false;
+            m1[s[i]] = t[i];
+            m2[t[i]] = s[i];
         }
         return true;
     }
