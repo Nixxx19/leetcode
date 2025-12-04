3adc0554d88bd52cd11fec19098238b9f5be7f6f
nityam <nixxspeaks@gmail.com>
2025-12-04 21:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.21%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & -n) == n;
+        if (n <= 0) return false;
+        return __builtin_popcount(n) == 1;
     }
 };
