619f3b7608a72458075b58cf9e92dd590a9c7474
nityam <nixxspeaks@gmail.com>
2025-12-08 23:07:38 +0530

    Time: 4 ms (14.92%) | Memory: 16.2 MB (98.95%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    vector<string> ans;
-    void dfs(TreeNode* n, string &path) {
-        if (!n->left && !n->right) {
-            ans.push_back(path);
-            return;
-        }
-        int old = path.size();
-        if (n->left) {
-            path += "->" + to_string(n->left->val);
-            dfs(n->left, path);
-            path.resize(old);
-        }
-        if (n->right) {
-            path += "->" + to_string(n->right->val);
-            dfs(n->right, path);
-            path.resize(old);
-        }
-    }
     vector<string> binaryTreePaths(TreeNode* root) {
-        string path = to_string(root->val);
-        dfs(root, path);
+        vector<string> ans;
+        queue<pair<TreeNode*,string>> q;
+        q.push({root,to_string(root->val)});
+        while (!q.empty()) {
+            auto [n,s] = q.front(); q.pop();
+            if (!n->left && !n->right) ans.push_back(s);
+            if (n->left) q.push({n->left, s + "->" + to_string(n->left->val)});
+            if (n->right) q.push({n->right, s + "->" + to_string(n->right->val)});
+        }
         return ans;
     }
 };
