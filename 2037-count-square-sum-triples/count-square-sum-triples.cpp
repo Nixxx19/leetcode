dbc74fb375e9c8608f5d609acfd915fa14368b07
nityam <nixxspeaks@gmail.com>
2025-12-08 23:05:43 +0530

    Time: 7 ms (62.17%) | Memory: 7.9 MB (55.75%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -3,9 +3,11 @@ public:
     int countTriples(int n) {
         int c = 0;
         for (int a = 1; a <= n; a++)
-            for (int b = 1; b <= n; b++)
-                for (int k = a*a + b*b, c2 = 1; c2 <= n; c2++)
-                    if (c2*c2 == k) c++;
+            for (int b = 1; b <= n; b++) {
+                int s = a*a + b*b;
+                int c2 = sqrt(s);
+                if (c2 <= n && c2*c2 == s) c++;
+            }
         return c;
     }
 };
