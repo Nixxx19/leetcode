b3af22e3ee5112569ac760732d656d9b73a00d60
nityam <nixxspeaks@gmail.com>
2025-12-09 23:12:02 +0530

    Time: 551 ms (77.37%) | Memory: 368 MB (64.60%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        int n = nums.size(), mod = 1000000007;
-        int mx = 100000;
-        vector<long long> left(mx+1), right(mx+1);
+        unordered_map<int,long long> left, right;
+        long long ans = 0, mod = 1000000007;
         for (int x : nums) right[x]++;
-        long long ans = 0;
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < nums.size(); j++) {
             int x = nums[j];
             right[x]--;
-            int t = x * 2;
-            if (t <= mx) ans = (ans + left[t] * right[t]) % mod;
+            long long t = x * 2;
+            if (left.count(t) && right.count(t))
+                ans = (ans + left[t] * right[t]) % mod;
             left[x]++;
         }
         return ans;
