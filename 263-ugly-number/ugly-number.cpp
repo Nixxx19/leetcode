ae812a13f96a2cf8d969e55c9f04b30777c354dd
nityam <nixxspeaks@gmail.com>
2025-12-11 23:15:42 +0530

    Time: 4 ms (1.86%) | Memory: 7.7 MB (79.86%) - LeetSync

--- 263-ugly-number/ugly-number.cpp
+++ 263-ugly-number/ugly-number.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool isUgly(int n) {
         if (n <= 0) return false;
-        while (n % 2 == 0) n /= 2;
-        while (n % 3 == 0) n /= 3;
-        while (n % 5 == 0) n /= 5;
+        for (int p : {2, 3, 5}) {
+            while (n % p == 0) n /= p;
+        }
         return n == 1;
     }
 };
