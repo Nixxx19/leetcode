0bcf1bdfb3ca71f1bf7320dc148fbd57b27d71df
nityam <nixxspeaks@gmail.com>
2025-12-13 23:47:30 +0530

    Time: 16 ms (92.36%) | Memory: 40.9 MB (75.00%) - LeetSync

--- 3721-count-mentions-per-user/count-mentions-per-user.cpp
+++ 3721-count-mentions-per-user/count-mentions-per-user.cpp
@@ -1,55 +1,47 @@
 class Solution {
 public:
-    vector<int> countMentions(int n, vector<vector<string>>& events) {
-        int N = events.size();
-        vector<int> ts(N);
-        for (int i = 0; i < N; i++) ts[i] = atoi(events[i][1].c_str());
-
+    vector<int> countMentions(int n, vector<vector<string>>& e) {
         vector<int> m(n), on(n,1), off(n);
-        vector<int> idx(N);
-        iota(idx.begin(), idx.end(), 0);
-
-        sort(idx.begin(), idx.end(), [&](int a, int b) {
-            return ts[a] < ts[b];
+        sort(e.begin(), e.end(), [](auto&a,auto&b){
+            return atoi(a[1].c_str()) < atoi(b[1].c_str());
         });
 
-        int i = 0;
-        while (i < N) {
-            int t = ts[idx[i]];
+        for (int i = 0; i < e.size();) {
+            int t = atoi(e[i][1].c_str());
+
             for (int u = 0; u < n; u++)
                 if (!on[u] && off[u] <= t) on[u] = 1;
 
             int j = i;
-            vector<int> offIdx, msgIdx;
-            while (j < N && ts[idx[j]] == t) {
-                if (events[idx[j]][0] == "OFFLINE") offIdx.push_back(idx[j]);
-                else msgIdx.push_back(idx[j]);
+            vector<int> a, b;
+            while (j < e.size() && atoi(e[j][1].c_str()) == t) {
+                (e[j][0] == "OFFLINE" ? a : b).push_back(j);
                 j++;
             }
 
-            for (int k : offIdx) {
-                int u = atoi(events[k][2].c_str());
+            for (int k : a) {
+                int u = atoi(e[k][2].c_str());
                 on[u] = 0;
                 off[u] = t + 60;
             }
 
-            for (int k : msgIdx) {
-                string &s = events[k][2];
+            for (int k : b) {
+                string &s = e[k][2];
                 if (s == "ALL") {
                     for (int u = 0; u < n; u++) m[u]++;
                 } else if (s == "HERE") {
                     for (int u = 0; u < n; u++) if (on[u]) m[u]++;
                 } else {
-                    string tok;
-                    stringstream ss(s);
-                    while (ss >> tok)
-                        m[atoi(tok.substr(2).c_str())]++;
+                    int x = -1;
+                    for (char c : s) {
+                        if (isdigit(c)) x = (x < 0 ? c - '0' : x * 10 + c - '0');
+                        else if (x >= 0) { m[x]++; x = -1; }
+                    }
+                    if (x >= 0) m[x]++;
                 }
             }
-
             i = j;
         }
-
         return m;
     }
 };
