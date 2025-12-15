b71d89a4616bd81f3f606922fbf875ce234c3e7a
nityam <nixxspeaks@gmail.com>
2025-12-15 21:00:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (37.03%) - LeetSync

--- 290-word-pattern/word-pattern.cpp
+++ 290-word-pattern/word-pattern.cpp
@@ -7,12 +7,17 @@ public:
         while (ss >> t) w.push_back(t);
         if (p.size() != w.size()) return false;
 
-        unordered_map<char,int> m1;
-        unordered_map<string,int> m2;
+        unordered_map<string,char> mp;
+        set<char> used;
 
         for (int i = 0; i < p.size(); i++) {
-            if (m1[p[i]] != m2[w[i]]) return false;
-            m1[p[i]] = m2[w[i]] = i + 1;
+            if (mp.count(w[i])) {
+                if (mp[w[i]] != p[i]) return false;
+            } else {
+                if (used.count(p[i])) return false;
+                mp[w[i]] = p[i];
+                used.insert(p[i]);
+            }
         }
         return true;
     }
