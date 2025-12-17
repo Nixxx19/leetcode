227ed930489fdeca463080939b5b7d8b380bb07e
nityam <nixxspeaks@gmail.com>
2025-12-17 23:13:35 +0530

    Time: 91 ms (16.93%) | Memory: 23.9 MB (92.46%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,34 +1,15 @@
 class NumArray {
 public:
-    vector<int> seg;
-    int n;
+    vector<int> nums;
 
-    NumArray(vector<int>& nums) {
-        n = nums.size();
-        seg.assign(4 * n, 0);
-        build(1, 0, n - 1, nums);
-    }
-
-    void build(int idx, int l, int r, vector<int>& nums) {
-        if (l == r) {
-            seg[idx] = nums[l];
-            return;
-        }
-        int m = (l + r) / 2;
-        build(idx * 2, l, m, nums);
-        build(idx * 2 + 1, m + 1, r, nums);
-        seg[idx] = seg[idx * 2] + seg[idx * 2 + 1];
-    }
-
-    int query(int idx, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l) return 0;
-        if (ql <= l && r <= qr) return seg[idx];
-        int m = (l + r) / 2;
-        return query(idx * 2, l, m, ql, qr)
-             + query(idx * 2 + 1, m + 1, r, ql, qr);
+    NumArray(vector<int>& arr) {
+        nums = arr;
     }
 
     int sumRange(int left, int right) {
-        return query(1, 0, n - 1, left, right);
+        int s = 0;
+        for (int i = left; i <= right; i++)
+            s += nums[i];
+        return s;
     }
 };
