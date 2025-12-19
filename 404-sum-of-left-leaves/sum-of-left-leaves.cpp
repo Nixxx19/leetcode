b794eb8d7ed05af783ca7a2a87e0ebf238f48269
nityam <nixxspeaks@gmail.com>
2025-12-19 23:25:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (23.92%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
         if (!root) return 0;
-        queue<pair<TreeNode*, bool>> q;
-        q.push({root, false});
+        stack<pair<TreeNode*, bool>> st;
+        st.push({root, false});
         int sum = 0;
-        while (!q.empty()) {
-            auto [node, isLeft] = q.front(); q.pop();
+        while (!st.empty()) {
+            auto [node, isLeft] = st.top(); st.pop();
             if (!node->left && !node->right && isLeft)
                 sum += node->val;
-            if (node->left) q.push({node->left, true});
-            if (node->right) q.push({node->right, false});
+            if (node->right) st.push({node->right, false});
+            if (node->left) st.push({node->left, true});
         }
         return sum;
     }
