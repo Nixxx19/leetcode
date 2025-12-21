a8bc126cfb20e79df701b58fefaf241e9b53a504
nityam <nixxspeaks@gmail.com>
2025-12-21 23:07:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (62.92%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int n = strs.size(), m = strs[0].size(), res = 0;
-        vector<bool> fixed(n - 1, false);
+        int n = strs.size(), m = strs[0].size();
+        vector<int> locked(n - 1, 0);
+        int ans = 0;
         for (int j = 0; j < m; j++) {
-            bool del = false;
+            bool remove = false;
             for (int i = 0; i < n - 1; i++) {
-                if (!fixed[i] && strs[i][j] > strs[i + 1][j]) {
-                    del = true;
+                if (!locked[i] && strs[i][j] > strs[i + 1][j]) {
+                    remove = true;
                     break;
                 }
             }
-            if (del) {
-                res++;
+            if (remove) {
+                ans++;
             } else {
                 for (int i = 0; i < n - 1; i++) {
-                    if (strs[i][j] < strs[i + 1][j]) fixed[i] = true;
+                    if (strs[i][j] < strs[i + 1][j]) locked[i] = 1;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
