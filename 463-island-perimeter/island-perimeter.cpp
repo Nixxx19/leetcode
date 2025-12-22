37ab80b0aae57a7645a1caf830a1b298c90df1c2
nityam <nixxspeaks@gmail.com>
2025-12-22 22:48:05 +0530

    Time: 33 ms (6.31%) | Memory: 107.5 MB (10.49%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int r = grid.size(), c = grid[0].size();
-        int per = 0;
+        vector<vector<int>> vis(r, vector<int>(c, 0));
         int d[5] = {0, 1, 0, -1, 0};
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j]) {
-                    for (int k = 0; k < 4; k++) {
-                        int ni = i + d[k], nj = j + d[k + 1];
-                        if (ni < 0 || nj < 0 || ni >= r || nj >= c || grid[ni][nj] == 0)
-                            per++;
-                    }
-                }
-            }
-        }
-        return per;
+
+        function<int(int,int)> dfs = [&](int i, int j) {
+            if (i < 0 || j < 0 || i >= r || j >= c || grid[i][j] == 0)
+                return 1;
+            if (vis[i][j]) return 0;
+            vis[i][j] = 1;
+            int res = 0;
+            for (int k = 0; k < 4; k++)
+                res += dfs(i + d[k], j + d[k + 1]);
+            return res;
+        };
+
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                if (grid[i][j])
+                    return dfs(i, j);
+
+        return 0;
     }
 };
