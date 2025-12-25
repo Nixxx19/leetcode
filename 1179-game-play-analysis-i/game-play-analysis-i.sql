d0683a9c9c4f79b0ccb7b8c7d8147e3706db436d
nityam <nixxspeaks@gmail.com>
2025-12-25 23:05:54 +0530

    Time: 463 ms (64.08%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,9 @@
-SELECT
-    player_id,
-    MIN(event_date) AS first_login
-FROM Activity
-GROUP BY player_id;
+SELECT a.player_id, a.event_date AS first_login
+FROM Activity a
+JOIN (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) b
+ON a.player_id = b.player_id
+AND a.event_date = b.first_login;
