c6db7ae8afa50b5df76c52eb1260f36a454cc895
nityam <nixxspeaks@gmail.com>
2025-12-26 23:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (13.39%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int absents = 0, lateStreak = 0;
+
+        for (char c : s) {
+            if (c == 'A') {
+                absents++;
+                lateStreak = 0;
+                if (absents >= 2) return false;
+            } 
+            else if (c == 'L') {
+                lateStreak++;
+                if (lateStreak >= 3) return false;
+            } 
+            else {
+                lateStreak = 0;
+            }
+        }
+        return true;
+    }
+};
