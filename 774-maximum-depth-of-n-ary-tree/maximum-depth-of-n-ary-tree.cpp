d4d40e5678ae73d5a9d9fcef3b3d5f68aa727e50
nityam <nixxspeaks@gmail.com>
2025-12-26 23:58:58 +0530

    Time: 16 ms (17.90%) | Memory: 14.8 MB (8.71%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -2,9 +2,19 @@ class Solution {
 public:
     int maxDepth(Node* root) {
         if (!root) return 0;
+        queue<Node*> q;
+        q.push(root);
         int depth = 0;
-        for (Node* child : root->children)
-            depth = max(depth, maxDepth(child));
-        return depth + 1;
+
+        while (!q.empty()) {
+            int sz = q.size();
+            depth++;
+            while (sz--) {
+                Node* cur = q.front(); q.pop();
+                for (Node* child : cur->children)
+                    q.push(child);
+            }
+        }
+        return depth;
     }
 };
