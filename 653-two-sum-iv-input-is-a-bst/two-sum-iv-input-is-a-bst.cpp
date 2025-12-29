108573d0fb6db8875f26f27d53c52a94c520edaa
nityam <nixxspeaks@gmail.com>
2025-12-29 22:54:53 +0530

    Time: 8 ms (25.92%) | Memory: 38.6 MB (39.59%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    stack<TreeNode*> l, r;
-
-    void pushLeft(TreeNode* n) {
-        while (n) {
-            l.push(n);
-            n = n->left;
-        }
-    }
-
-    void pushRight(TreeNode* n) {
-        while (n) {
-            r.push(n);
-            n = n->right;
-        }
-    }
-
     bool findTarget(TreeNode* root, int k) {
-        pushLeft(root);
-        pushRight(root);
+        unordered_set<int> s;
+        queue<TreeNode*> q;
+        q.push(root);
 
-        while (!l.empty() && !r.empty() && l.top() != r.top()) {
-            int a = l.top()->val;
-            int b = r.top()->val;
-            if (a + b == k) return true;
-            if (a + b < k) {
-                TreeNode* n = l.top(); l.pop();
-                pushLeft(n->right);
-            } else {
-                TreeNode* n = r.top(); r.pop();
-                pushRight(n->left);
-            }
+        while (!q.empty()) {
+            auto n = q.front(); q.pop();
+            if (s.count(k - n->val)) return true;
+            s.insert(n->val);
+            if (n->left) q.push(n->left);
+            if (n->right) q.push(n->right);
         }
         return false;
     }
