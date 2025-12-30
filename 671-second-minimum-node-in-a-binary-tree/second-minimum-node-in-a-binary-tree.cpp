b0e5c4c260cf08c592f1db66e211d4231b987a40
nityam <nixxspeaks@gmail.com>
2025-12-30 23:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.00%) - LeetSync

--- 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int helper(TreeNode* node, int base) {
-        if (!node) return -1;
-        if (node->val > base) return node->val;
-
-        int l = helper(node->left, base);
-        int r = helper(node->right, base);
-
-        if (l == -1) return r;
-        if (r == -1) return l;
-        return min(l, r);
+    void dfs(TreeNode* node, set<int>& s) {
+        if (!node) return;
+        s.insert(node->val);
+        dfs(node->left, s);
+        dfs(node->right, s);
     }
 
     int findSecondMinimumValue(TreeNode* root) {
-        return helper(root, root->val);
+        set<int> s;
+        dfs(root, s);
+        if (s.size() < 2) return -1;
+        auto it = s.begin();
+        it++;
+        return *it;
     }
 };
