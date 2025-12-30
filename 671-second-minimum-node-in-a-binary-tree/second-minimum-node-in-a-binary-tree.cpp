2c54f2f8909d7d3e5cee9f6489ed7a0dc40a4d6a
nityam <nixxspeaks@gmail.com>
2025-12-30 23:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (49.00%) - LeetSync

--- 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void dfs(TreeNode* node, set<int>& s) {
-        if (!node) return;
-        s.insert(node->val);
-        dfs(node->left, s);
-        dfs(node->right, s);
-    }
-
     int findSecondMinimumValue(TreeNode* root) {
-        set<int> s;
-        dfs(root, s);
-        if (s.size() < 2) return -1;
-        auto it = s.begin();
-        it++;
-        return *it;
+        long long first = root->val, second = LLONG_MAX;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            auto cur = q.front(); q.pop();
+            if (cur->val > first && cur->val < second)
+                second = cur->val;
+            if (cur->left) q.push(cur->left);
+            if (cur->right) q.push(cur->right);
+        }
+
+        return second == LLONG_MAX ? -1 : second;
     }
 };
