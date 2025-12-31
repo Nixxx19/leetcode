2de6364a6ac0f42ce251fa3cecb33537bb7abf14
nityam <nixxspeaks@gmail.com>
2025-12-31 23:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (61.85%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    bool isPal(string &s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) return false;
-        }
-        return true;
-    }
-
     bool validPalindrome(string s) {
         int l = 0, r = s.size() - 1;
         while (l < r) {
             if (s[l] != s[r]) {
-                return isPal(s, l + 1, r) || isPal(s, l, r - 1);
+                int i = l + 1, j = r;
+                while (i < j && s[i] == s[j]) i++, j--;
+                if (i >= j) return true;
+
+                i = l; j = r - 1;
+                while (i < j && s[i] == s[j]) i++, j--;
+                return i >= j;
             }
             l++; r--;
         }
