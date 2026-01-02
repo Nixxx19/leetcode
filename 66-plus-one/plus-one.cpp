2c854add80e7b2ff7afc0af037472b54e74a7e0d
nityam <nixxspeaks@gmail.com>
2026-01-02 09:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (50.22%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        reverse(digits.begin(), digits.end());
-        int carry = 1, i = 0;
-        while (i < digits.size() && carry) {
-            int sum = digits[i] + carry;
-            digits[i] = sum % 10;
-            carry = sum / 10;
-            i++;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        if (carry) digits.push_back(carry);
-        reverse(digits.begin(), digits.end());
+        digits.insert(digits.begin(), 1);
         return digits;
     }
 };
