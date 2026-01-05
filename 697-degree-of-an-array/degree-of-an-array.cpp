9e8312beb8bbda0c6e6d3c98d61b2eb14c2d5625
nityam <nixxspeaks@gmail.com>
2026-01-05 23:22:43 +0530

    Time: 3 ms (94.99%) | Memory: 30.2 MB (32.09%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.cpp
+++ 697-degree-of-an-array/degree-of-an-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int,int> cnt, l;
-        int ans = nums.size(), deg = 0;
+        unordered_map<int,int> cnt, l, r;
         for (int i = 0; i < nums.size(); i++) {
             if (!l.count(nums[i])) l[nums[i]] = i;
+            r[nums[i]] = i;
             cnt[nums[i]]++;
-            if (cnt[nums[i]] > deg) {
-                deg = cnt[nums[i]];
-                ans = i - l[nums[i]] + 1;
-            } else if (cnt[nums[i]] == deg) {
-                ans = min(ans, i - l[nums[i]] + 1);
-            }
         }
+        int deg = 0, ans = nums.size();
+        for (auto &p : cnt) deg = max(deg, p.second);
+        for (auto &p : cnt)
+            if (p.second == deg)
+                ans = min(ans, r[p.first] - l[p.first] + 1);
         return ans;
     }
 };
