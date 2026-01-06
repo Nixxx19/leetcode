0c33d82df3f0ce723b1b53aa5d006ac98f318313
nityam <nixxspeaks@gmail.com>
2026-01-06 23:28:49 +0530

    Time: 12 ms (10.80%) | Memory: 107.2 MB (93.11%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        vector<long long> lvl;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int sz = q.size();
-            long long s = 0;
-            for (int i = 0; i < sz; i++) {
-                auto n = q.front(); q.pop();
-                s += n->val;
-                if (n->left) q.push(n->left);
-                if (n->right) q.push(n->right);
+        unordered_map<int,long long> mp;
+        int mx = 1;
+        function<void(TreeNode*,int)> dfs = [&](TreeNode* n, int d) {
+            if (!n) return;
+            mp[d] += n->val;
+            mx = max(mx, d);
+            dfs(n->left, d + 1);
+            dfs(n->right, d + 1);
+        };
+        dfs(root, 1);
+        long long best = LLONG_MIN;
+        int ans = 1;
+        for (int i = 1; i <= mx; i++) {
+            if (mp[i] > best) {
+                best = mp[i];
+                ans = i;
             }
-            lvl.push_back(s);
         }
-        int idx = 0;
-        for (int i = 1; i < lvl.size(); i++)
-            if (lvl[i] > lvl[idx]) idx = i;
-        return idx + 1;
+        return ans;
     }
 };
