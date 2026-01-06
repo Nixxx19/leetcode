2c0292098a515807db43eb4c0115cad2e87e0106
nityam <nixxspeaks@gmail.com>
2026-01-06 23:36:11 +0530

    Time: 16 ms (51.45%) | Memory: 47.6 MB (85.73%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,14 +1,24 @@
 class MyHashSet {
-    vector<bool> v;
+    static const int B = 1000;
+    vector<vector<int>> h;
 public:
-    MyHashSet() : v(1000001, false) {}
+    MyHashSet() : h(B) {}
+
     void add(int key) {
-        v[key] = true;
+        int i = key % B;
+        for (int x : h[i]) if (x == key) return;
+        h[i].push_back(key);
     }
+
     void remove(int key) {
-        v[key] = false;
+        int i = key % B;
+        auto &b = h[i];
+        b.erase(std::remove(b.begin(), b.end(), key), b.end());
     }
+
     bool contains(int key) {
-        return v[key];
+        int i = key % B;
+        for (int x : h[i]) if (x == key) return true;
+        return false;
     }
 };
