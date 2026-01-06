d64844a1c81e1d88085b5661d3caabab705ec28f
nityam <nixxspeaks@gmail.com>
2026-01-06 23:34:16 +0530

    Time: 8 ms (85.53%) | Memory: 50.5 MB (28.19%) - LeetSync

---
+++ 816-design-hashset/design-hashset.cpp
@@ -0,0 +1,14 @@
+class MyHashSet {
+    vector<bool> v;
+public:
+    MyHashSet() : v(1000001, false) {}
+    void add(int key) {
+        v[key] = true;
+    }
+    void remove(int key) {
+        v[key] = false;
+    }
+    bool contains(int key) {
+        return v[key];
+    }
+};
