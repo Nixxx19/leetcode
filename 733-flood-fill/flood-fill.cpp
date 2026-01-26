e97421b416a5b9f7367795ad706870291b381211
nityam <nixxspeaks@gmail.com>
2026-01-26 23:12:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (31.51%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -4,17 +4,17 @@ public:
         int orig=image[sr][sc];
         if(orig==color) return image;
         int m=image.size(), n=image[0].size();
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
+        stack<pair<int,int>> st;
+        st.push({sr,sc});
         image[sr][sc]=color;
         int d[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
-        while(!q.empty()){
-            auto [x,y]=q.front(); q.pop();
+        while(!st.empty()){
+            auto [x,y]=st.top(); st.pop();
             for(auto& k:d){
                 int nx=x+k[0], ny=y+k[1];
                 if(nx>=0&&ny>=0&&nx<m&&ny<n&&image[nx][ny]==orig){
                     image[nx][ny]=color;
-                    q.push({nx,ny});
+                    st.push({nx,ny});
                 }
             }
         }
