25cb7e86a94df728dc5df1cd874074a25fe95d5c
nityam <nixxspeaks@gmail.com>
2026-01-26 23:14:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (99.53%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        auto it=upper_bound(letters.begin(),letters.end(),target);
-        return it==letters.end()?letters[0]:*it;
+        for(char c:letters)
+            if(c>target) return c;
+        return letters[0];
     }
 };
