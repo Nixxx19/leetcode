05a0494813c43fc2b8774071487ccba2061bfbd4
nityam <nixxspeaks@gmail.com>
2026-01-27 23:26:36 +0530

    Time: 15 ms (33.33%) | Memory: 25.6 MB (5.03%) - LeetSync

--- 749-shortest-completing-word/shortest-completing-word.cpp
+++ 749-shortest-completing-word/shortest-completing-word.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     string shortestCompletingWord(string licensePlate, vector<string>& words) {
         vector<int> cnt(26, 0);
-        for (char c : licensePlate) {
+        for (char c : licensePlate)
             if (isalpha(c)) cnt[tolower(c) - 'a']++;
-        }
 
         string res;
+        int minLen = INT_MAX;
+
+        vector<vector<int>> wordCounts;
         for (auto &word : words) {
-            vector<int> wc(26, 0);
+            vector<int> wc(26,0);
             for (char c : word) wc[c - 'a']++;
+            wordCounts.push_back(wc);
+        }
 
+        for (int i = 0; i < words.size(); i++) {
             bool ok = true;
-            for (int i = 0; i < 26; i++) {
-                if (cnt[i] > wc[i]) {
-                    ok = false;
-                    break;
-                }
-            }
-
-            if (ok && (res.empty() || word.size() < res.size())) {
-                res = word;
+            for (int j = 0; j < 26; j++)
+                if (cnt[j] > wordCounts[i][j]) { ok = false; break; }
+            if (ok && words[i].size() < minLen) {
+                minLen = words[i].size();
+                res = words[i];
             }
         }
         return res;
