35a20a6d93635835b961c02e97ad378f5dabeae7
nityam <nixxspeaks@gmail.com>
2026-01-27 23:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (48.77%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        auto it = max_element(nums.begin(), nums.end());
-        int maxVal = *it;
-        int idx = distance(nums.begin(), it);
+        pair<int,int> maxPair = {-1, -1}; // {value, index}
+        int secondMax = -1;
         for (int i = 0; i < nums.size(); i++) {
-            if (i != idx && maxVal < 2 * nums[i]) return -1;
+            if (nums[i] > maxPair.first) {
+                secondMax = maxPair.first;
+                maxPair = {nums[i], i};
+            } else if (nums[i] > secondMax) {
+                secondMax = nums[i];
+            }
         }
-        return idx;
+        return maxPair.first >= 2 * secondMax ? maxPair.second : -1;
     }
 };
