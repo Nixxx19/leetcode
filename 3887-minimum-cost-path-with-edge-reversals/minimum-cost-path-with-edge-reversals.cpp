a1f1ace986b7cd5e3ad98b408bd2d0b25ecf2f4b
nityam <nixxspeaks@gmail.com>
2026-01-27 23:15:09 +0530

    Time: 510 ms (17.78%) | Memory: 337.6 MB (19.95%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     int minCost(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int,int>>> g(2 * n);
-
+        vector<vector<pair<int,int>>> g(n), rg(n);
         for (auto &e : edges) {
-            int u = e[0], v = e[1], w = e[2];
-            g[u].push_back({v, w});
-            g[u + n].push_back({v + n, w});
-            g[v].push_back({u + n, 2 * w});
-            g[v + n].push_back({u + n, 2 * w});
+            g[e[0]].push_back({e[1], e[2]});
+            rg[e[1]].push_back({e[0], e[2]});
         }
 
         const long long INF = 1e18;
-        vector<long long> dist(2 * n, INF);
-        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> pq;
+        vector<vector<long long>> dist(n, vector<long long>(2, INF));
+        priority_queue<array<long long,3>, vector<array<long long,3>>, greater<>> pq;
 
-        dist[0] = 0;
-        pq.push({0, 0});
+        dist[0][0] = 0;
+        pq.push({0, 0, 0});
 
         while (!pq.empty()) {
-            auto [d, u] = pq.top(); pq.pop();
-            if (d != dist[u]) continue;
+            auto [d, u, mode] = pq.top();
+            pq.pop();
+            if (d != dist[u][mode]) continue;
+
             for (auto &[v, w] : g[u]) {
-                if (dist[v] > d + w) {
-                    dist[v] = d + w;
-                    pq.push({dist[v], v});
+                if (dist[v][mode] > d + w) {
+                    dist[v][mode] = d + w;
+                    pq.push({dist[v][mode], v, mode});
+                }
+            }
+
+            for (auto &[v, w] : rg[u]) {
+                long long nd = d + 2LL * w;
+                if (dist[v][1] > nd) {
+                    dist[v][1] = nd;
+                    pq.push({nd, v, 1});
                 }
             }
         }
 
-        long long ans = min(dist[n - 1], dist[n - 1 + n]);
+        long long ans = min(dist[n - 1][0], dist[n - 1][1]);
         return ans >= INF ? -1 : (int)ans;
     }
 };
