2444073e4f976d982f587a47a335b9b9202ed9c7
nityam <nixxspeaks@gmail.com>
2026-01-30 23:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (77.62%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        int cnt[128] = {};
-        for (char c : stones) cnt[c]++;
         int ans = 0;
-        for (char c : jewels) ans += cnt[c];
+        for (char s : stones)
+            for (char j : jewels)
+                if (s == j) ans++;
         return ans;
     }
 };
