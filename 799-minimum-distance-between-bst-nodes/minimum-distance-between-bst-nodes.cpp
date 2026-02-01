ee11ffc31e5677f90fc37181bf310f30b4ef213a
Nixxx19 <nixxspeaks@gmail.com>
2026-02-01 22:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (23.28%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    vector<int> v;
-
-    void dfs(TreeNode* root) {
-        if (!root) return;
-        dfs(root->left);
-        v.push_back(root->val);
-        dfs(root->right);
-    }
-
     int minDiffInBST(TreeNode* root) {
-        dfs(root);
-        int ans = INT_MAX;
-        for (int i = 1; i < v.size(); i++)
-            ans = min(ans, v[i] - v[i - 1]);
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        int prev = -1, ans = INT_MAX;
+
+        while (cur || !st.empty()) {
+            while (cur) {
+                st.push(cur);
+                cur = cur->left;
+            }
+            cur = st.top(); st.pop();
+            if (prev != -1)
+                ans = min(ans, cur->val - prev);
+            prev = cur->val;
+            cur = cur->right;
+        }
         return ans;
     }
 };
