20e4c2d3a9796960083792068d0636fd4c0c0d92
Nixxx19 <nixxspeaks@gmail.com>
2026-02-12 23:22:59 +0530

    Time: 67 ms (92.99%) | Memory: 25.4 MB (52.24%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     int longestBalanced(string s) {
-        int n = s.size();
-        int ans = 0;
+        int n = s.size(), ans = 0;
 
         for(int i = 0; i < n; i++) {
             vector<int> freq(26, 0);
-            int distinct = 0, maxFreq = 0;
+            int distinct = 0;
 
             for(int j = i; j < n; j++) {
                 int idx = s[j] - 'a';
                 if(freq[idx] == 0) distinct++;
                 freq[idx]++;
-                maxFreq = max(maxFreq, freq[idx]);
 
                 int len = j - i + 1;
-                if(len == distinct * maxFreq)
-                    ans = max(ans, len);
+                if(len % distinct == 0) {
+                    int target = len / distinct;
+                    bool ok = true;
+
+                    for(int k = 0; k < 26; k++) {
+                        if(freq[k] > 0 && freq[k] != target) {
+                            ok = false;
+                            break;
+                        }
+                    }
+
+                    if(ok) ans = max(ans, len);
+                }
             }
         }
-
         return ans;
     }
 };
