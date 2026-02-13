d2e12f8844c59fcdf270ab9ede5566073a88e293
nityam <nixxspeaks@gmail.com>
2026-02-13 23:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (6.69%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
         int n = s.length();
-        vector<int> res(n);
-        int pos = -10001;
+        vector<int> res(n, -1);
+        queue<int> q;
         for (int i = 0; i < n; ++i) {
-            if (s[i] == c) pos = i;
-            res[i] = i - pos;
+            if (s[i] == c) {
+                res[i] = 0;
+                q.push(i);
+            }
         }
-        for (int i = pos - 1; i >= 0; --i) {
-            if (s[i] == c) pos = i;
-            res[i] = min(res[i], pos - i);
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            for (int next : {cur - 1, cur + 1}) {
+                if (next >= 0 && next < n && res[next] == -1) {
+                    res[next] = res[cur] + 1;
+                    q.push(next);
+                }
+            }
         }
         return res;
     }
