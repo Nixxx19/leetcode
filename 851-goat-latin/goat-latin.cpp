cca7805d53060fd339b5725fe7b7e42db0ca8e16
nityam <nixxspeaks@gmail.com>
2026-02-13 23:59:29 +0530

    Time: 2 ms (36.22%) | Memory: 9 MB (75.72%) - LeetSync

--- 851-goat-latin/goat-latin.cpp
+++ 851-goat-latin/goat-latin.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     string toGoatLatin(string sentence) {
-        string res, vowels = "aeiouAEIOU";
-        int count = 0, n = sentence.length();
-        for (int i = 0; i < n; ++i) {
-            int j = i;
-            while (j < n && sentence[j] != ' ') j++;
-            string word = sentence.substr(i, j - i);
-            if (vowels.find(word[0]) == string::npos) {
-                word = word.substr(1) + word[0];
-            }
-            res += word + "ma" + string(++count, 'a') + (j < n ? " " : "");
-            i = j;
+        string res;
+        string_view sv = sentence;
+        string vowels = "aeiouAEIOU";
+        int idx = 1;
+        size_t pos = 0, next;
+        while ((next = sentence.find(' ', pos)) != string::npos || pos < sentence.size()) {
+            string word = (next == string::npos) ? (string)sv.substr(pos) : (string)sv.substr(pos, next - pos);
+            if (vowels.find(word[0]) == string::npos) rotate(word.begin(), word.begin() + 1, word.end());
+            res += word + "ma" + string(idx++, 'a') + " ";
+            if (next == string::npos) break;
+            pos = next + 1;
         }
+        if (!res.empty()) res.pop_back();
         return res;
     }
 };
