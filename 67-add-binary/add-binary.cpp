91ad01022ec36d7e5e39eb52bf3a14f8e1a867e3
nityam <nixxspeaks@gmail.com>
2026-02-15 23:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (36.83%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n = max(a.size(), b.size());
-        a = string(n - a.size(), '0') + a;
-        b = string(n - b.size(), '0') + b;
         string res = "";
-        int carry = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            int sum = (a[i] - '0') + (b[i] - '0') + carry;
-            res += (sum % 2) + '0';
+        int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            res.push_back((sum % 2) + '0');
             carry = sum / 2;
         }
-        if (carry) res += '1';
         reverse(res.begin(), res.end());
         return res;
     }
