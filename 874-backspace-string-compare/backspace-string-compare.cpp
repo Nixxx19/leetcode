6874cfaf3eced09a136d13e77c5402b3d26c8899
nityam <nixxspeaks@gmail.com>
2026-02-16 23:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.78%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int i = s.size() - 1, j = t.size() - 1;
-        int skipS = 0, skipT = 0;
-        
-        while (i >= 0 || j >= 0) {
-            while (i >= 0) {
-                if (s[i] == '#') { skipS++; i--; }
-                else if (skipS > 0) { skipS--; i--; }
-                else break;
-            }
-            
-            while (j >= 0) {
-                if (t[j] == '#') { skipT++; j--; }
-                else if (skipT > 0) { skipT--; j--; }
-                else break;
-            }
-            
-            if (i >= 0 && j >= 0) {
-                if (s[i] != t[j]) return false;
-            } else {
-                if (i >= 0 || j >= 0) return false;
-            }
-            
-            i--;
-            j--;
+        int i = 0;
+        for (char c : s) {
+            if (c != '#') s[i++] = c;
+            else if (i > 0) i--;
         }
+        s.resize(i);
         
-        return true;
+        i = 0;
+        for (char c : t) {
+            if (c != '#') t[i++] = c;
+            else if (i > 0) i--;
+        }
+        t.resize(i);
+        
+        return s == t;
     }
 };
