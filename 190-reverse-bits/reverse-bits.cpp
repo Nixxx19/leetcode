7c4a93710663b19bc3f3a7a91eb04dbf13b0ea1c
nityam <nixxspeaks@gmail.com>
2026-02-16 23:07:30 +0530

    Time: 4 ms (29.28%) | Memory: 8.3 MB (15.72%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        n = (n >> 16) | (n << 16);
-        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
-        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
-        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
-        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
-        return n;
+        static uint32_t table[256];
+        static bool initialized = false;
+        
+        if (!initialized) {
+            for (int i = 0; i < 256; i++) {
+                uint32_t x = i, r = 0;
+                for (int j = 0; j < 8; j++) {
+                    r = (r << 1) | (x & 1);
+                    x >>= 1;
+                }
+                table[i] = r;
+            }
+            initialized = true;
+        }
+        
+        return (table[n & 0xff] << 24) |
+               (table[(n >> 8) & 0xff] << 16) |
+               (table[(n >> 16) & 0xff] << 8) |
+               (table[(n >> 24) & 0xff]);
     }
 };
