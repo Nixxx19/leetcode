f0a977fa9cbad9c2b7d0da182e2b64940f78b2fe
nityam <nixxspeaks@gmail.com>
2026-02-17 23:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (41.32%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    vector<string> ans;
-    void dfs(int idx, int cnt, int h, int m, int k) {
-        if (h >= 12 || m >= 60) return;
-        if (cnt == k) {
-            string s = to_string(h) + ":";
-            if (m < 10) s += "0";
-            s += to_string(m);
-            ans.push_back(s);
-            return;
-        }
-        if (idx == 10) return;
-        if (idx < 4) dfs(idx + 1, cnt + 1, h | (1 << idx), m, k);
-        else dfs(idx + 1, cnt + 1, h, m | (1 << (idx - 4)), k);
-        dfs(idx + 1, cnt, h, m, k);
-    }
     vector<string> readBinaryWatch(int turnedOn) {
-        dfs(0, 0, 0, 0, turnedOn);
-        return ans;
+        vector<string> res;
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                    string time = to_string(h) + ":";
+                    if (m < 10) time += "0";
+                    time += to_string(m);
+                    res.push_back(time);
+                }
+            }
+        }
+        return res;
     }
 };
