6fdc7df7946afa70d36da6733cccfb61efaa29d3
nityam <nixxspeaks@gmail.com>
2026-02-17 23:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (41.32%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
+    int countBits(int x) {
+        int cnt = 0;
+        while (x) {
+            cnt += x & 1;
+            x >>= 1;
+        }
+        return cnt;
+    }
+    
     vector<string> readBinaryWatch(int turnedOn) {
         vector<string> res;
         for (int h = 0; h < 12; h++) {
             for (int m = 0; m < 60; m++) {
-                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                if (countBits(h) + countBits(m) == turnedOn) {
                     string time = to_string(h) + ":";
                     if (m < 10) time += "0";
                     time += to_string(m);
